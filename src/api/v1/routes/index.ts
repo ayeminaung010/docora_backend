1e07ece979d764f2afbce76bda31ae69393453f2
ayeminaung010 <ayeminaung.mf@gmail.com>
2025-07-27 15:26:19 +0700

    feat: auth feature start

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import userRouter from "./user.routes";
+import authRouter from "./auth.routes";
 
 const router = Router();
+router.use("/auth",authRouter)
 router.use("/patients", userRouter);
 // router.use("/doctors", doctorRouter);
-
 export default router;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,1 +1,21 @@
-// Handles password hashing, token generation
+import { signUp } from './../api/v1/controllers/auth.controller';
+import { ApiResponse } from "../utils/ApiResponse";
+
+export interface loginRequest{
+    email : string;
+    password: string;
+    rememberMe?: boolean;
+}
+export interface signUpRequest{
+    email: string;
+    password: string;
+    name: string;
+}
+export class AuthService {
+    private static readonly JWT_SECRET = process.env.JWT_SECRET || "irene_secret";
+    private static readonly JWT_EXPIRATION = process.env.JWT_EXPIRATION || "1h";
+    static async signUp(req: signUpRequest) {
+        
+        return "User signup logic not implemented yet";
+    }
+}
