b2af62ce5cdecdc5ea390661c66672ae77b5b803
irene-love-bluesea <eaindraychu32@gmail.com>
2025-07-28 21:10:37 +0700

    feat: PatientUpdate

--- src/api/v1/controllers/patient.controller.ts
+++ src/api/v1/controllers/patient.controller.ts
@@ -3,14 +3,38 @@ import { ApiResponse } from "../../../utils/ApiResponse";
 import { ApiError } from "../../../utils/ApiError";
 import { asyncHandler } from "../../../utils/asyncHandler";
 import { PatientService } from "../../../services/patient.service";
+import { AuthenticatedRequest } from "../middlewares/auth.middleware";
 
-export const patientDetailForm = asyncHandler(async(req: Request, res: Response)=>{
-    const result = await PatientService.patientDetailForm(req.body);
-    return res.status(200).json(new ApiResponse(200, "Patient Data Saved Successfully"));
+export const patientDetailForm = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user?.userId;
+    if (!userId) {
+      return res
+        .status(400)
+        .json(new ApiResponse(400, {}, "User ID is required"));
+    }
 
-});
+    const result = await PatientService.patientDetailForm(userId, req.body);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Patient Data Saved Successfully"));
+  }
+);
 
-export const patientInfoUpdate = asyncHandler(async(req: Request, res: Response) => {
-    const result = await PatientService.patientInfoUpdate(req.body);
-    return res.status(200).json(new ApiResponse(200, result, "Patient Information Updated Successfully"));
-}); 
+export const patientInfoUpdate = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user?.userId;
+    if (!userId) {
+      return res
+        .status(400)
+        .json(new ApiResponse(400, {}, "User ID is required"));
+    }
+
+    const result = await PatientService.patientInfoUpdate(userId, req.body);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, result, "Patient Information Updated Successfully")
+      );
+  }
+);
--- src/api/v1/routes/patient.route.ts
+++ src/api/v1/routes/patient.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { patientDetailForm, patientInfoUpdate } from "../controllers/patient.controller";
+import {
+  patientDetailForm,
+  patientInfoUpdate,
+} from "../controllers/patient.controller";
+import { authenticate } from "../middlewares/auth.middleware";
 
-const router=Router();
+const router = Router();
+router.use(authenticate);
+
 router.post("/patientDetailForm", patientDetailForm);
 router.patch("/patientInfoUpdate", patientInfoUpdate);
-
 
 export default router;
