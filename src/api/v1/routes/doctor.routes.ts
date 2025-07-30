6271d2f041b4f94b5a41bb555ba081e320a28e16
ayeminaung010 <ayeminaung.mf@gmail.com>
2025-07-30 18:28:14 +0700

    feat: define url

--- src/api/v1/routes/doctor.routes.ts
+++ src/api/v1/routes/doctor.routes.ts
@@ -6,12 +6,33 @@ import { viewScheduleDoctor } from "../controllers/schedule.controller";
 
 
 const router = Router();
+
 router.use(authenticate);
+// for identity page 
+router.post("/verifyIdentity" , verifyIdentityDoctor );
+
+//for home page
+// router.get("/home", getHomeData) // home page
+// router.get("/notificatons", getNotifications) // go to noti page
+
+//for appointment page
+// router.get("/consulstaions/upcoming", getUpcomingConsulations)
+// router.get("/consulstaions/past", getPastConsulations)
+// router.get("/consulstaions/filter", getFilteredConsulations)
 router.get("/schedule/:doctorId", viewScheduleDoctor);
+// router.post("/schedule/create", createSchedule)
+// router.post("/schedule/delete", deleteSchedule)
+
+//for chat page 
+// router.get("/chats", getChats)
+// router.get("/chats/:chatId", viewChat)
+
+//for patient consultation note 
+// router.get("/patientNote/:patientId", viewPatientNote)
 
 // router.use(authorize(["DOCTOR"]));
 
-router.post("/verifyIdentity" , verifyIdentityDoctor );
+//for profile
 router.patch("/profile/update",  profileUpdate );
 router.get("/popularDoctors", getPopularDoctors);
 router.get("/user/details/:id", viewUserDetails);
--- src/api/v1/routes/patient.route.ts
+++ src/api/v1/routes/patient.route.ts
@@ -13,4 +13,24 @@ router.use(authenticate);
 router.post("/patientDetailForm", patientDetailForm);
 router.patch("/patientInfoUpdate", patientProfileUpdate);
 
+// for home page
+
+// for appointment page
+// router.get("/consultaions/upcoming", getUpcomingConsulations)
+// router.get("/consultaions/past", getPastConsulations)
+// router.get("/consultaions/filter", getFilteredConsulations)
+
+//for chat page
+// router.get("/chats", getChats)
+// router.get("/chats/:chatId", viewChat)
+
+//for profile page
+
+
+// router.get('doctor/:id', viewDoctorProfile);
+
+// for booking appointment
+// router.get("/consultaions/booking", createConsulation)
+
+
 export default router;
